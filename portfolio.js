01ca290a78df26c8f111bc1ac5dc7cdf1233663e
waelghozzi232 <waelghozzi232@gmail.com>
2023-06-02 21:51:21 +0100

    dynamic design

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,28 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Your code here
 
-const button = document.querySelectorAll('.link');
+
+const button1 = document.querySelector('.link1');
+const button2 = document.querySelector('.link2');
 const gal = document.querySelector('.gal');
+const letters = document.querySelector('.letters');
 
-button.forEach((e)=>{
-    e.addEventListener("click", ()=>{
+    button1.addEventListener("click", ()=>{
+        letters.style.display = 'block';
+});
+
+button2.addEventListener("click", ()=>{
     gal.style.display = 'block';
 });
-})
+
+
+
 
 gal.addEventListener("click", ()=>{
     gal.style.display = 'none';
-});  });
+});  
+letters.addEventListener("click", ()=>{
+    letters.style.display = 'none';
+});  
+});
